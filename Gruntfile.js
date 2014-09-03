7eb81185da2a8bbc9d2f5844bb6dc6eec18fa762
Clay Miller <clay@smockle.com>
2014-09-03 00:14:59 -0500

    Remove unsupported option (SASS sourcemaps).

--- Gruntfile.js
+++ Gruntfile.js
@@ -152,7 +152,7 @@ module.exports = function (grunt) {
     // Compiles Sass to CSS and generates necessary files if requested
     sass: {
       options: {
-        sourcemap: true,
+//        sourcemap: true,
         loadPath: 'bower_components'
       },
       dist: {
